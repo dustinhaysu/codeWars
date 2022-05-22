63186408c3530e539c836d627b03be1b688b990d
Dustin Hays <hays.dustin.u@gmail.com>
2022-05-21 22:37:28 -0700

    commit

--- 07disemvowelTrolls/js/main.js
+++ 07disemvowelTrolls/js/main.js
@@ -2,4 +2,4 @@ function disemvowel(str){
     return str.replace(/[aeiou]/gi , '')
   }
   
-  console.log(disemvowel('Regular expressions are like camping with the in-laws!!!'))
+  console.log(disemvowel('There once was a man from Nantucket!!!'))
