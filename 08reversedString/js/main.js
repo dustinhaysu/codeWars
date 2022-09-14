24f28f1e2821f00b216471fb02f0045e4fa0d191
Dustin Hays <hays.dustin.u@gmail.com>
2022-09-13 22:13:26 -0700

    ReversedStrings

---
+++ 08reversedString/js/main.js
@@ -0,0 +1,11 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+// STRINGSFUNDAMENTALS
+
+function solution(str){
+ return str.split("").reverse().join('')
+}
+
+console.log(solution("Dustin"))
