d0759954be82c10b359c035c9d87e997b710f3f5
Dustin Hays <hays.dustin.u@gmail.com>
2022-11-27 05:55:22 -0800

    07SortArray-1

---
+++ 07SortArray-1/js/main.js
@@ -0,0 +1,14 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// SORTING ARRAYS FUNDAMENTALS
+
+let names = ['Car', 'Bunny', 'for', 'Letter']
+
+
+// input: names - unsorted array
+// output: sorted array
+sortMe = function( names ){
+    return names.sort()
+  }
+
+  console.log(sortMe(names))
